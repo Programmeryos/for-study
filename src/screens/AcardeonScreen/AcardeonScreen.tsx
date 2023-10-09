9db79b3b5ada24008f31fcf1f968d56518eeedb0
Yura <yos.bobik@gmail.com>
2023-10-09 12:23:06 +0300

    accordion finish

--- src/screens/AcardeonScreen/AcardeonScreen.tsx
+++ src/screens/AcardeonScreen/AcardeonScreen.tsx
@@ -1,15 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
-import Animated from 'react-native-reanimated'
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  Easing,
+  useAnimatedRef,
+  useDerivedValue,
+  runOnUI,
+  measure,
+} from 'react-native-reanimated'
+import { ZoomIn, ZoomOut } from 'react-native-reanimated'
+
+import { LayoutChangeEvent } from 'react-native'
 
 import { RootStackScreenProps } from '../../navigation/types'
 
+import styles from './styles'
+
 type Props = RootStackScreenProps<'home'>
 const AcardeonScreen: React.FC<Props> = ({ navigation }) => {
+  const listRef = useAnimatedRef<Animated.View>()
+  const heightValue = useSharedValue(0)
+  const open = useSharedValue(false)
+  const progress = useDerivedValue(() =>
+    open.value ? withTiming(1) : withTiming(0)
+  )
+
+  const heightAnimationStyle = useAnimatedStyle(() => ({
+    height: heightValue.value,
+  }))
+
   return (
-    <SafeAreaView>
-      <Text>AcardeonScreen</Text>
+    <SafeAreaView className="px-[24]">
+      <View style={styles.container}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            if (heightValue.value === 0) {
+              runOnUI(() => {
+                'worklet'
+                heightValue.value = withTiming(measure(listRef)!.height)
+              })()
+            } else {
+              heightValue.value = withTiming(0)
+            }
+            open.value = !open.value
+          }}
+        >
+          <View style={styles.titleContainer}>
+            <Text style={styles.textTitle}>Hello World!</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <Animated.View className="bg-red-400" style={heightAnimationStyle}>
+          <Animated.View style={styles.contentContainer} ref={listRef}>
+            <Text>
+              Hello wrold is Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Magnam amet nam dolorem veniam commodi, odio rerum ad,
+              dolorum repellendus adipisci necessitatibus, fugiat harum? Dolorum
+              libero quos alias quae sunt culpa!
+            </Text>
+          </Animated.View>
+        </Animated.View>
+      </View>
     </SafeAreaView>
   )
 }
--- src/screens/AcardeonScreen/styles.ts
+++ src/screens/AcardeonScreen/styles.ts
@@ -1,5 +1,36 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-  //
+  container: {
+    backgroundColor: '#E3EDFB',
+    marginHorizontal: 10,
+    marginVertical: 10,
+    borderRadius: 14,
+    borderWidth: 1,
+    borderColor: '#0F56B3',
+    overflow: 'hidden',
+  },
+  textTitle: {
+    fontSize: 16,
+    color: 'black',
+  },
+  titleContainer: {
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+  },
+  content: {
+    padding: 20,
+    backgroundColor: '#D6E1F0',
+  },
+  textContent: {
+    fontSize: 14,
+    color: 'black',
+  },
 })
